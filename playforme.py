8c2211d861c0b32b61dcb3a10c653d92708ced8a
Шарунов Владислав <Newrussian73@mail.ru>
2022-10-04 15:03:58 +0400

    просто в комит

--- playforme.py
+++ playforme.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import unittest
 
 
-link="https://a.nc01.mptuz.crpt.tech/auth"
+
 
 #Авторизация в админ-панели
-try:
+def auth(link):
     browser=webdriver.Chrome()
     browser.get(link)
     loginadmin=browser.find_element(By.XPATH, '//*[@id="Login"]')
@@ -16,6 +17,17 @@ try:
     passwordadmin.send_keys('Blokpost1976!')
     buttonlogin=browser.find_element(By.CSS_SELECTOR, "button.btn")
     buttonlogin.click()
-    time.sleep(5)
-finally:
-    browser.quit()
+    if browser.current_url != 'https://a.nc01.mptuz.crpt.tech/':
+
+        passwordadmin=browser.find_element(By.XPATH, '//*[@id="Password"]')
+        passwordadmin.send_keys('Blokpost1976!')
+        buttonlogin=browser.find_element(By.CSS_SELECTOR, "button.btn")
+        buttonlogin.click()
+        time.sleep(5)
+    url=browser.current_url
+    return url
+class testauth(unittest.TestCase):
+    def testexpurl(self):
+        self.assertEqual(auth('https://a.nc01.mptuz.crpt.tech/auth'),'https://a.nc01.mptuz.crpt.tech/','not auth')
+if __name__=="__main__":
+    unittest.main()
